366a603be21fdb9cb18e9788070eee496dd4b646
DMD <daodu.muyiwa@gmail.com>
2020-05-14 17:15:27 -0600

    Updates

---
+++ precache-manifest.a9a0ce6284f42715695eb445c1ddd60a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a98b710450191a90570edb9c419ca73b",
+    "url": "/cardList/index.html"
+  },
+  {
+    "revision": "6a42bb8456e432d41c25",
+    "url": "/cardList/static/css/main.3104c5b8.chunk.css"
+  },
+  {
+    "revision": "87f27d2ecf92509876b3",
+    "url": "/cardList/static/js/2.697cfb5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cardList/static/js/2.697cfb5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a42bb8456e432d41c25",
+    "url": "/cardList/static/js/main.b7de5f59.chunk.js"
+  },
+  {
+    "revision": "6900a6a7555a34762c85",
+    "url": "/cardList/static/js/runtime-main.571d712b.js"
+  }
+]);
